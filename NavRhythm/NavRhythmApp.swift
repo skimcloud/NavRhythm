927c319c8e40b4bfe300d3352517696f1cb7a09c
skimcloud <skiim@ucdavis.edu>
2024-02-21 21:00:08 -0800

    Initial commit

---
+++ NavRhythm/NavRhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavRhythmApp.swift
+//  NavRhythm
+//
+//  Created by Shane Kim on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NavRhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
