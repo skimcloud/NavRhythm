0a41a9d82152cd199c71273f5a6fe10e415eddd2
dulce <torres1010200@gmail.com>
2024-03-01 13:26:34 -0800

    Add search bar UI and used mapkit to get locations from database. Grabs locations that are simliar to either start or desination text input

---
+++ NavRhythmPrototype/NavRhythmPrototype/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  NavRhythmPrototype
+//
+//  Created by Dulce & Flor on 3/1/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
