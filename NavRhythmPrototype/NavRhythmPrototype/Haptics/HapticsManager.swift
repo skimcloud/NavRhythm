538f91400441546146b86ae9e96b9134d9de5f39
Jen Galicia <jfgalicia@ucdavis.edu>
2024-03-09 15:50:14 -0800

    HapticsManager Class

--- NavRhythmPrototype/NavRhythmPrototype/Haptics/HapticsManager.swift
+++ NavRhythmPrototype/NavRhythmPrototype/Haptics/HapticsManager.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+fileprivate final class HapticsManager {
+    
+    static let shared = HapticsManager()
+    
+    private let feedback = UINotificationFeedbackGenerator()
+    
+    private init() {}
+    
+    func trigger(_ notification: UINotificationFeedbackGenerator.FeedbackType) {
+        feedback.notificationOccurred(notification)
+    }
+    
+}
+
+func haptic(_ notification: UINotificationFeedbackGenerator.FeedbackType) {
+    HapticsManager.shared.trigger(notification)
+}
