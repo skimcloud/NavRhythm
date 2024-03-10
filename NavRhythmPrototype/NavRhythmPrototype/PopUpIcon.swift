3b616a59811b756a4bdf24e03f5457db779283c8
dulce <torres1010200@gmail.com>
2024-03-10 01:36:39 -0800

    Added vibration UI and button, add icon of app, added pop up welcome to app page

---
+++ NavRhythmPrototype/NavRhythmPrototype/PopUpIcon.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpIcon.swift
+//  NavRhythmPrototype
+//
+//  Created by Dulce & Flor on 3/10/24.
+//
+
+import SwiftUI
+
+struct PopUpIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopUpIcon()
+}
---
+++ NavRhythmPrototype/NavRhythmPrototype/vibration.swift
@@ -0,0 +1,18 @@
+//
+//  vibration.swift
+//  NavRhythmPrototype
+//
+//  Created by Dulce & Flor on 3/10/24.
+//
+
+import SwiftUI
+
+struct vibration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    vibration()
+}
